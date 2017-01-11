cbafa97fe3fc87326a932f3c4b5d2008d9b2ba66
Joseph Mugwaneza <muganezajoseph@gmail.com>
2017-01-11 14:36:22 +0200

    printed item3 color

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,5 @@ item1 = {product: "rice", color: "white", price: 800}
 item2 = {product: "pancake", color: "brown", price: 100}
 
 item3 = {product: "computer", color: "purple", price: 1250}
+
+puts item3[:color]
