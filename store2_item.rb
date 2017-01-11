5d47ffbe86300366c3f0af0de163198b468ca5a7
Joseph Mugwaneza <muganezajoseph@gmail.com>
2017-01-11 16:03:58 +0200

    added other codes

---
+++ store2_item.rb
@@ -0,0 +1,35 @@
+class Store
+
+	def initialize (item_name, item_color, item_price)
+		@product_name = item_name
+		@item_color = item_color
+		@item_price = item_price
+
+		def item_name
+			@product_name
+		end 
+
+		def item_color=(item_color)
+			@item_color = item_color
+		end 
+
+		def item_price=(item_price)
+			@item_price = item_price
+		end 
+
+		def info
+			"#{item_name} #{@item_color} #{@item_price}"
+		end 
+
+	end 
+end 
+
+item1 =Store.new("rice", "white", 800)
+item2 =Store.new("pancake", "brown", 100)
+item3 =Store.new("computer", "purple", 1250)
+
+item2.item_price=(550)
+item3.item_color=("Yellow")
+
+puts item3.info
+
--- store_item.rb
+++ store_item.rb
@@ -4,4 +4,4 @@ item2 = {product: "pancake", color: "brown", price: 100}
 
 item3 = {product: "computer", color: "purple", price: 1250}
 
-puts item3[:color]
+puts item2[:color]
